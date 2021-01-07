6f8b5b1d3c1397d970ad2f74c105d2a2a0058e39
Oliver Aco <benavidez.jemver@gmail.com>
2021-01-08 00:30:45 +0800

    Updates

---
+++ precache-manifest.569cb8142cbd340bcda06f488d7c66d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a7d7981f57299ab99b42dbb97ed5a98",
+    "url": "/calculator-app/index.html"
+  },
+  {
+    "revision": "08bbff4fac72a0c7cf66",
+    "url": "/calculator-app/static/js/2.3ae4d809.chunk.js"
+  },
+  {
+    "revision": "a397e2e228b6a47d56b473e22166c81a",
+    "url": "/calculator-app/static/js/2.3ae4d809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "892fd185c8649ef1b6b2",
+    "url": "/calculator-app/static/js/main.9a29a4dc.chunk.js"
+  },
+  {
+    "revision": "0d5b87d0df9303af2826",
+    "url": "/calculator-app/static/js/runtime-main.013694fb.js"
+  }
+]);
